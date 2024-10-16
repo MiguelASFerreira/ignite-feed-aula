f11e4d12b3ebd6d9bf60aa10b2d78a8de2c12c6e
MiguelASFerreira <migant1306@gmail.com>
2024-10-16 17:46:20 -0300

    Adicionando keys

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -51,9 +51,9 @@ export function Post({ author, content, publishedAt}) {
             <div className={styles.content}>
                 {content.map(line => {
                     if (line.type === "paragraph") {
-                        return <p>{line.content}</p>
+                        return <p key={line.type}>{line.content}</p>
                     } else if (line.type === "link") {
-                        return <p><a href="#">{line.content}</a></p>
+                        return <p key={line.type}><a href="#">{line.content}</a></p>
                     }
                 })}
             </div>
@@ -75,7 +75,7 @@ export function Post({ author, content, publishedAt}) {
 
             <div className={styles.commentList}>
                 {comments.map((comment) => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
             </div>
         </article>
