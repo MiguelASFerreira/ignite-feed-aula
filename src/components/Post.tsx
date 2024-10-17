cccba0d3f09eaf5674428b1f1fd23c1749076117
MiguelASFerreira <migant1306@gmail.com>
2024-10-16 21:01:27 -0300

    Configurando typescript

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,30 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 import { format, formatDistanceToNow } from "date-fns";
-import ptBR from "date-fns/locale/pt-BR";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
-import styles from "./Post.module.css";
-import { useState } from "react";
 
-export function Post({ author, content, publishedAt }) {
+import styles from "./Post.module.css";
+
+type Author = {
+  name: string;
+  role: string;
+  avatarUrl: string
+}
+
+type Content = {
+  type: "paragraph" | "link";
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+export function Post({ author, content, publishedAt }: PostProps) {
   const [comments, setComments] = useState(["Post muito bacana, hein?"]);
 
   const [newCommentText, setNewCommentText] = useState("");
@@ -24,23 +42,23 @@ export function Post({ author, content, publishedAt }) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment() {
+  function handleCreateNewComment(event: FormEvent) {
     event.preventDefault();
 
     setComments([...comments, newCommentText]);
     setNewCommentText("");
   }
 
-  function handleNewCommentChange() {
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("")
     setNewCommentText(event.target.value);
   }
 
-  function handleNewCommentInvalid() {
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
     event.target.setCustomValidity("Esse campo é obrigatório!")
   }
 
-  function deleteComment(commentToDelete) {
+  function deleteComment(commentToDelete: string) {
     const commentWithoutDeletedOne = comments.filter(comment => {
       return comment !== commentToDelete
     })
